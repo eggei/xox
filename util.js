3c846b972fa1c2f4314bd50d1966a5a65eeb490c
eggei <macitegeercan@gmail.com>
2020-06-14 21:57:48 -0400

    styles

--- util.js
+++ util.js
@@ -180,7 +180,6 @@ function printResult(winner) {
 }
 
 function paintBoard() {
-  console.log(board);
   for (let i = 0; i < 3; ++i) {
     for (let j = 0; j < 3; ++j) {
       if (board[i][j] === player.human.sign) {
